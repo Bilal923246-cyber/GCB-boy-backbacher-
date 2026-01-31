8a1507454e0ed5fde793a514e63ce34352457616
Bilal923246-cyber <hafizmuhammadbilal295@gmail.com>
2026-01-31 05:32:32 +0500

    Create bilal.cpp

---
+++ bilal.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namepace  std;
+int  main(void){
+   cout << " my nam  is cyber gcb world and i start this journey and becoming the c++  programming and i will share with  you the complex problems,,,, as well as the biggner friendly projects that you ca make and strengthen you portfolio  an dwe have a
+a strong community name as cyber gcb world community join this commuity as soon as possible and you will get the valuable insights ""  << endl;
+
+return 0 ; 
+}
